83e17c1bdba3703b5ffc59cfacd7166a692c3814
zxh <agrainofsand2011@126.com>
2016-12-30 14:20:41 +0800

    first

---
+++ MEFTest/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MEFTest
+{
+    class Program
+    {
+        private CompositionContainer _container;
+
+        [Import(typeof(ICalculator))]
+        public ICalculator calculator;
+
+
+        private Program()
+        {
+            //An aggregate catalog that combines multiple catalogs
+            var catalog = new AggregateCatalog();
+            //Adds all the parts found in the same assembly as the Program class
+            catalog.Catalogs.Add(new AssemblyCatalog(typeof(Program).Assembly));
+            catalog.Catalogs.Add(new DirectoryCatalog("C:\\Users\\SomeUser\\Documents\\Visual Studio 2010\\Projects\\SimpleCalculator3\\SimpleCalculator3\\Extensions"));
+
+
+            //Create the CompositionContainer with the parts in the catalog
+            _container = new CompositionContainer(catalog);
+
+            //Fill the imports of this object
+            try
+            {
+                this._container.ComposeParts(this);
+            }
+            catch (CompositionException compositionException)
+            {
+                Console.WriteLine(compositionException.ToString());
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            Program p = new Program(); //Composition is performed in the constructor
+            String s;
+            Console.WriteLine("Enter Command:");
+            while (true)
+            {
+                s = Console.ReadLine();
+                Console.WriteLine(p.calculator.Calculate(s));
+            }
+
+
+        }
+    }
+}
---
+++ MEFTest/TestClass.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MEFTest
+{
+    public interface ICalculator
+    {
+        String Calculate(String input);
+    }
+
+    public interface IOperation
+    {
+        int Operate(int left, int right);
+    }
+
+    public interface IOperationData
+    {
+        Char Symbol { get; }
+    }
+
+    [Export(typeof(IOperation))]
+    [ExportMetadata("Symbol", '+')]
+    class Add : IOperation
+    {
+        public int Operate(int left, int right)
+        {
+            return left + right;
+        }
+    }
+
+    [Export(typeof(IOperation))]
+    [ExportMetadata("Symbol", '-')]
+    class Subtract : IOperation
+    {
+
+        public int Operate(int left, int right)
+        {
+            return left - right;
+        }
+
+    }
+
+
+
+    [Export(typeof(ICalculator))]
+    class MySimpleCalculator : ICalculator
+    {
+        [ImportMany]
+        IEnumerable<Lazy<IOperation, IOperationData>> operations;
+
+        public String Calculate(String input)
+        {
+            int left;
+            int right;
+            Char operation;
+            int fn = FindFirstNonDigit(input); //finds the operator
+            if (fn < 0) return "Could not parse command.";
+
+            try
+            {
+                //separate out the operands
+                left = int.Parse(input.Substring(0, fn));
+                right = int.Parse(input.Substring(fn + 1));
+            }
+            catch
+            {
+                return "Could not parse command.";
+            }
+
+            operation = input[fn];
+
+            foreach (Lazy<IOperation, IOperationData> i in operations)
+            {
+                if (i.Metadata.Symbol.Equals(operation)) return i.Value.Operate(left, right).ToString();
+            }
+            return "Operation Not Found!";
+        }
+
+        private int FindFirstNonDigit(String s)
+        {
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (!(Char.IsDigit(s[i]))) return i;
+            }
+            return -1;
+        }
+
+
+    }
+}
